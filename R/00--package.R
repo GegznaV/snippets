e1a2e1192f1de9e650e7d5b7ff90646415099632
Vilmantas Gegzna <GegznaV@gmail.com>
2020-01-04 21:49:11 +0200

    Create 00--package.R

---
+++ R/00--package.R
@@ -0,0 +1,12 @@
+#' @name snippets
+#' @docType package
+#'
+#' @title Manage RStudio snippets
+#'
+#' @description
+#' Tools to manage RStudio snippets.
+#' Package \pkg{snippets} also contains RStudio snippets.
+#'
+#' @importFrom magrittr "%>%"
+
+NULL
