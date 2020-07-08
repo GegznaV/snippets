056515633ba8af52a47c0305a88d32e82527ab4a
Vilmantas Gegzna <GegznaV@gmail.com>
2020-07-08 20:59:46 +0300

    Move code to reexports.R

---
+++ R/reexports.R
@@ -0,0 +1,20 @@
+# @name edit_rstudio_snippets
+# @title Edit file with RStudio snippets.
+#
+# @description
+# Open and edit file with RStudio snippets.
+# This function is imported from package \pkg{usethis}.
+#
+# @details
+# Files created by `edit_rstudio_snippets()`` will mask, not supplement,
+# the built-in default snippets. If you like the built-in snippets, copy
+# them and include with your custom snippets.
+#
+# @inheritParams match_snippet_type
+#
+# @seealso
+# [usethis::edit_rstudio_snippets()]
+
+#' @importFrom usethis edit_rstudio_snippets
+#' @export
+usethis::edit_rstudio_snippets
