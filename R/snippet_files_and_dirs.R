d865563ab718cb5df625d39193bd3277332ce359
Vilmantas Gegzna <GegznaV@gmail.com>
2020-07-08 20:13:44 +0300

    Move code to snippet_files_and_dirs.R

---
+++ R/snippet_files_and_dirs.R
@@ -0,0 +1,127 @@
+# ======================================================================== ~~~~
+# Files and directories for snippets ------------------------------------- ====
+# ======================================================================== ~~~~
+
+#' Make a filename for snippets.
+#'
+#' Make a filename for certain type of RStudio snippets.
+#'
+#' @inheritParams match_snippet_type
+#'
+#' @return String with a filename.
+#' @export
+#'
+#' @concept snippet files and dirs
+#' @examples
+#' make_snippet_filename()
+#'
+#' make_snippet_filename("markdown")
+#'
+#' make_snippet_filename(c("r", "markdown"), several.ok = TRUE)
+
+make_snippet_filename <- function(type = get_default_snippet_types(),
+  several.ok = FALSE) {
+  type <- match_snippet_type(type, several.ok = several.ok)
+  stringr::str_glue("{type}.snippets")
+}
+
+#' Get path to directory for RStudio snippets.
+#'
+#' @return (string) Directory name.
+#' @export
+#'
+#' @concept snippet files and dirs
+#' @examples
+#' get_rs_snippets_dir()
+
+get_rs_snippets_dir <- function() {
+  fs::path_expand_r("~/.R/snippets/")
+}
+
+#' Create directory for RStudio snippets.
+#'
+#' @return Invisibly returns the path to created directory.
+#'         See also [fs::dir_create()].
+#' @export
+#'
+#' @concept snippet files and dirs
+#' @examples
+#' if (FALSE) {
+#' create_rs_snippets_dir()
+#' }
+
+create_rs_snippets_dir <- function() {
+  fs::dir_create(get_rs_snippets_dir())
+}
+
+#' Open directory of RStudio snippets.
+#' @export
+#' @importFrom utils browseURL
+#' @concept snippet files and dirs
+
+open_rs_snippets_dir <- function() {
+  create_rs_snippets_dir()
+  browseURL(get_rs_snippets_dir())
+}
+
+#' Construct path to file of certain type snippets.
+#'
+#'  Construct path to file of certain type snippets:
+#'
+#' - `get_path_to_snippet_file()`    in any folder
+#' - `get_path_to_rs_snippet_file()` in RS snippets folder
+#'
+#' @inheritParams match_snippet_type
+#'
+#' @param dir (string) Directory name.
+#' @param create (logical) If `TRUE`, as a side effect, the file is created
+#'        (if it does not exist).
+#'
+#' @return (character) Path to file.
+#' @export
+#' @concept snippet files and dirs
+
+get_path_to_snippet_file <- function(dir, type = get_default_snippet_types(),
+  create = FALSE, several.ok = FALSE) {
+
+  paths <-
+    fs::path(dir, make_snippet_filename(type = type, several.ok = several.ok))
+
+  if (isTRUE(create)) {
+    for (path_i in paths) {
+      if (!file.exists(path_i)) {
+        fs::dir_create(fs::path_dir(path_i))
+        fs::file_create(path_i)
+      }
+    }
+  }
+
+  paths
+}
+
+#' @rdname get_path_to_snippet_file
+#' @export
+get_path_to_rs_snippet_file <- function(type = get_default_snippet_types(),
+  create = FALSE, several.ok = FALSE) {
+
+  get_path_to_snippet_file(dir = get_rs_snippets_dir(), type = type,
+    create = create, several.ok = several.ok)
+}
+
+
+#' Does file with snippets exist?
+#'
+#' Does a file with certain types of snippets exist in RStudio snippets directory?
+#'
+#' @inheritParams match_snippet_type
+#'
+#' @return Returns `TRUE` if file exists and `FALSE` otherwise.
+#' @export
+#'
+#' @concept snippet files and dirs
+#' @examples
+#' snippets_file_exists("r")
+#' snippets_file_exists("markdown")
+snippets_file_exists <- function(type) {
+  fs::file_exists(get_path_to_rs_snippet_file(type, create = FALSE))
+}
