6a22ee0347cf300edc240f3189b37dba77029d8f
Vilmantas Gegzna <GegznaV@gmail.com>
2020-08-26 17:17:25 +0300

    Rename file

--- R/snippets-package.R
+++ R/snippets-package.R
@@ -2,22 +2,25 @@
 #' @docType package
 #' @concept package
 #'
-#' @title Manage RStudio snippets
+#' @title Manage RStudio Snippets
 #'
 #' @description
+#' \lifecycle{experimental}
 #' Tools to manage RStudio snippets.
 #' Package \pkg{snippets} also contains RStudio snippets.
-#'
-#' @importFrom dplyr "%>%"
-#' @import backup.tools
 
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @import backup.tools
+#' @importFrom dplyr "%>%"
+#' @importFrom lifecycle deprecate_soft
 ## usethis namespace: end
 NULL
+
+
 
 
 globalVariables(c("data", "empty", "keyword", "line_type", "name", "no",
