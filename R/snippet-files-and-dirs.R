100e331d8a551a3d663be8749e5bb8858ce94e63
Vilmantas Gegzna <GegznaV@gmail.com>
2020-08-26 16:43:58 +0300

    REname function

--- R/snippet-files-and-dirs.R
+++ R/snippet-files-and-dirs.R
@@ -170,13 +170,13 @@ path_to_rs_snippets_file <- function(type = get_default_snippet_types(),
 #' @examples
 #' \dontrun{\donttest{
 #' # By defaults, edits R snippets
-#' edit_rstudio_snippets_file()
-#' edit_rstudio_snippets_file("r")
+#' open_rstudio_snippets_file()
+#' open_rstudio_snippets_file("r")
 #'
-#' edit_rstudio_snippets_file("markdown")
+#' open_rstudio_snippets_file("markdown")
 #' }}
 #'
-edit_rstudio_snippets_file <- function(type = get_default_snippet_types(),
+open_rstudio_snippets_file <- function(type = get_default_snippet_types(),
                                        rstudio_version = "auto") {
   rstudioapi::navigateToFile(
     path_to_rs_snippets_file(type = type, rstudio_version = rstudio_version)
