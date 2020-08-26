452f9e762d1d549f108fda49761b61aac6d1a6fe
Vilmantas Gegzna <GegznaV@gmail.com>
2020-08-26 15:05:45 +0300

    Update package's documentation

--- R/00--package.R
+++ R/00--package.R
@@ -11,4 +11,14 @@
 #' @importFrom dplyr "%>%"
 #' @import backup.tools
 
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
 NULL
+
+
+globalVariables(c("data", "empty", "keyword", "line_type", "name", "no",
+  "snippet", "snippet_beggins"))
