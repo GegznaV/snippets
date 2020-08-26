f3e9738e48ed8c6808d51270b91bd02be5625c3e
Vilmantas Gegzna <GegznaV@gmail.com>
2020-08-26 15:23:26 +0300

    Update test-snippets_file_exists.R

--- tests/testthat/test-snippets_file_exists.R
+++ tests/testthat/test-snippets_file_exists.R
@@ -1,6 +1,6 @@
 test_that("snippets_file_exists() works", {
   # Correct output class
-  expect_is(snippets_file_exists("r",   rstudio_version = "1.3.1073"),   "logical")
+  expect_is(snippets_file_exists("r",   rstudio_version = "1.3.1073"), "logical")
   expect_is(snippets_file_exists("css", rstudio_version = "1.3.1073"), "logical")
 
   # Incorrect snippet type
